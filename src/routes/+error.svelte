af41e16dd8fef6a910cecbb9ce82a5864dd5810e
Yousef Jalali <yousefjalalidev@gmail.com>
2024-09-25 10:56:08 +0300

    add styling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,5 +1,17 @@
-<script lang="ts">
+<!-- <script lang="ts">
 	import { page } from '$app/stores'
-</script>
+</script> -->
 
-<h1>go back <a href="/">back</a></h1>
+<div class="w-full flex justify-center items-center">
+	<div class="grid h-screen place-content-center px-4">
+		<div class="text-center">
+			<h1 class="text-9xl font-black">404</h1>
+
+			<p class="text-2xl font-bold tracking-tight sm:text-4xl opacity-60">Uh-oh!</p>
+
+			<p class="mt-4 opacity-60">We can't find that page.</p>
+
+			<a href="/" class="mt-6 btn btn-primary"> Go Back Home </a>
+		</div>
+	</div>
+</div>
